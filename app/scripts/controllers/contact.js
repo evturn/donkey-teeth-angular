0fcadff770c12575b43c4ade23495792ff14896f
Evan Turner <evturn@gmail.com>
2015-02-27 13:46:15 -0500

    contact page implemented

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name donkeyTeethApp.controller:ContactCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the donkeyTeethApp
+ */
+ angular.module('donkeyTeethApp')
+	.controller('ContactCtrl', function($scope) {
+		$scope.message = 'Come on over sometime, have a beer! I\'ll make you a sandwich with cheese in it.';
+ });
